88e3023415e79efdb6cc9b1644d6305d8842e85a
arcana426 <164586096+arcana426@users.noreply.github.com>
2025-01-02 02:23:13 +0900

    Create user-info.js

---
+++ js/user-info.js
@@ -0,0 +1,10 @@
+// Display user information on the home page
+function displayUserInfo(user) {
+    document.getElementById('user-info').innerHTML = `
+        <figure class="image is-128x128 is-inline-block">
+            <img class="is-rounded" src="${user.picture}" alt="User Image">
+        </figure>
+        <h2 class="title is-3">${user.name}</h2>
+        <p class="subtitle is-5">${user.email}</p>
+    `;
+}
